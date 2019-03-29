779f488f12edb41f34ac45e342204f7065878e72
박경욱 <kwpark@kwpark-MacBook-pro.local>
2019-03-30 08:16:42 +0900

    Added HelloController and updated readme document accordingly

--- src/main/kotlin/kr/co/redbrush/microservice/app/controller/HelloController.kt
+++ src/main/kotlin/kr/co/redbrush/microservice/app/controller/HelloController.kt
@@ -1,6 +1,7 @@
 package kr.co.redbrush.microservice.app.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseBody
@@ -9,5 +10,9 @@ import org.springframework.web.bind.annotation.ResponseBody
 class HelloController {
     @RequestMapping("/hello", method = [RequestMethod.GET])
     @ResponseBody
-    fun hello() = "hello kotlin microservice"
+    fun hello() = "Hello kotlin microservice!"
+
+    @RequestMapping("/hello/{name}", method = [RequestMethod.GET])
+    @ResponseBody
+    fun hello(@PathVariable name: String) = "Hello $name!"
 }
