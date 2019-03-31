389b58dab204af19a69e3c96d5ed0a0e0dfcdaae
박경욱 <kwpark@kwpark-MacBook-pro.local>
2019-03-31 23:59:26 +0900

    Added Account data class Added AccountController and getAccount endpoint Declared account companion object in the application

--- src/main/kotlin/kr/co/redbrush/microservice/app/MicroserviceDemoApplication.kt
+++ src/main/kotlin/kr/co/redbrush/microservice/app/MicroserviceDemoApplication.kt
@@ -3,12 +3,16 @@ package kr.co.redbrush.microservice.app
 import kr.co.redbrush.microservice.app.data.Account
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class MicroserviceDemoApplication {
 	companion object {
-	    val testAccount = Account("test", "1234");
+	    val testAccount = Account("test", "1234")
 	}
+
+	@Bean
+	fun account() = testAccount
 }
 
 fun main(args: Array<String>) {
