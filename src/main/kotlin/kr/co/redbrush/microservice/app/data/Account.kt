129f4351672a495ec4fcb6486424d2afbd975eab
박경욱 <kwpark@kwpark-MacBook-pro.local>
2019-04-15 22:41:57 +0900

    Added Jackson JSON Include setting

--- src/main/kotlin/kr/co/redbrush/microservice/app/data/Account.kt
+++ src/main/kotlin/kr/co/redbrush/microservice/app/data/Account.kt
@@ -1,6 +1,3 @@
 package kr.co.redbrush.microservice.app.data
 
-import com.fasterxml.jackson.annotation.JsonInclude
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
 data class Account(var id: String = "", var password: String = "", var telephone: Telephone? = null)
