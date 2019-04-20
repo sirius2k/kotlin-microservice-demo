286fbddcacb08065c4414ed212d8137f5e070743
박경욱 <kwpark@kwpark-MacBook-pro.local>
2019-04-20 15:40:36 +0900

    Added non-blocking reactive static content with WebFlux/Netty

--- src/main/kotlin/kr/co/redbrush/microservice/app/controller/ExceptionHandler.kt
+++ src/main/kotlin/kr/co/redbrush/microservice/app/controller/ExceptionHandler.kt
@@ -1,16 +1,11 @@
 package kr.co.redbrush.microservice.app.controller
 
-import com.fasterxml.jackson.core.JsonParseException
-import kr.co.redbrush.microservice.app.data.ErrorResponse
-import kr.co.redbrush.microservice.app.exception.AccountNotFoundException
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
-import org.springframework.web.bind.annotation.ExceptionHandler
-import javax.servlet.http.HttpServletRequest
 
 @ControllerAdvice
 class ErrorHandler {
+    // TODO : Change servlet based exception handler with WebFlux
+    /*
     @ExceptionHandler(JsonParseException::class)
     fun jsonParseExceptionHandler(servletRequest: HttpServletRequest, exception: Exception) : ResponseEntity<ErrorResponse> {
         return ResponseEntity(ErrorResponse("JSON Error", exception.message ?: "Invalid Json"), HttpStatus.BAD_REQUEST)
@@ -20,4 +15,5 @@ class ErrorHandler {
     fun accountNotFoundExceptionHandler(servletRequest: HttpServletRequest, exception: Exception) : ResponseEntity<ErrorResponse> {
         return ResponseEntity(ErrorResponse("Account not found", exception.message!!), HttpStatus.NOT_FOUND)
     }
+    */
 }
