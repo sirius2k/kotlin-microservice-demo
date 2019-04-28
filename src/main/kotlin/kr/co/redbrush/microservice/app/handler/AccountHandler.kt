fda71c472f6cbf277375d0910a5ed1c1966ed3fc
박경욱 <kwpark@kwpark-MacBook-pro.local>
2019-04-28 17:56:25 +0900

    Changed accountService to receive id parameter and return empty Mono if the result is empty

--- src/main/kotlin/kr/co/redbrush/microservice/app/handler/AccountHandler.kt
+++ src/main/kotlin/kr/co/redbrush/microservice/app/handler/AccountHandler.kt
@@ -1,16 +1,17 @@
 package kr.co.redbrush.microservice.app.handler
 
-import kr.co.redbrush.microservice.app.data.Account
+import kr.co.redbrush.microservice.app.service.AccountService
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.BodyInserters.fromObject
 import org.springframework.web.reactive.function.server.ServerRequest
-import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.ServerResponse.ok
-import reactor.core.publisher.Mono
-import reactor.core.publisher.toMono
+import org.springframework.web.reactive.function.server.ServerResponse.status
 
 @Component
-class AccountHandler {
-    fun get(serverRequest: ServerRequest): Mono<ServerResponse> {
-        return ok().body(Account("test", "password").toMono(), Account::class.java)
-    }
+class AccountHandler(val accountService: AccountService) {
+    fun get(serverRequest: ServerRequest) =
+            accountService.getAccount(serverRequest.pathVariable("id"))
+                    .flatMap { ok().body(fromObject(it)) }
+                    .switchIfEmpty(status(HttpStatus.NOT_FOUND).build())
 }
