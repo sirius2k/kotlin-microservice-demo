79d3fa324ae1f5e1a6d6298e152795907fafed9e
박경욱 <kwpark@kwpark-MacBook-pro.local>
2019-05-23 23:16:30 +0900

    Set Account as Document class and added Reactive Repository interface for Account

--- src/main/kotlin/kr/co/redbrush/microservice/app/data/Account.kt
+++ src/main/kotlin/kr/co/redbrush/microservice/app/data/Account.kt
@@ -1,3 +1,6 @@
 package kr.co.redbrush.microservice.app.data
 
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document(collection = "Accounts")
 data class Account(var id: String = "", var password: String = "", var telephone: Telephone? = null)
