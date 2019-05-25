5a73f0cb606600a028520838175696121dca8357
박경욱 <kwpark@kwpark-MacBook-pro.local>
2019-05-25 12:03:23 +0900

    Added Kotlin logging alternative library

--- src/main/kotlin/kr/co/redbrush/microservice/app/config/MongoConfig.kt
+++ src/main/kotlin/kr/co/redbrush/microservice/app/config/MongoConfig.kt
@@ -2,10 +2,13 @@ package kr.co.redbrush.microservice.app.config
 
 import kr.co.redbrush.microservice.app.data.Account
 import kr.co.redbrush.microservice.app.repository.AccountRepository
+import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.core.ReactiveMongoOperations
 import javax.annotation.PostConstruct
+
+private val logger = KotlinLogging.logger {}
 
 @Configuration
 class MongoConfig {
@@ -20,14 +23,20 @@ class MongoConfig {
         mongoOperations.collectionExists("Accounts").subscribe {
             if (it != true) {
                 mongoOperations.createCollection("Accounts").subscribe {
-                    println("Accounts collection created.")
+                    logger.info { "Accounts collection created." }
                 }
             } else {
-                println("Accounts collections already exist.")
+                logger.info {"Accounts collections already exist." }
             }
 
-            accountRepository.save(Account("test", "test")).subscribe {
-                println("Test account created.")
+            accountRepository.existsById("test").subscribe {
+                if (it != true) {
+                    accountRepository.save(Account("test", "test")).subscribe {
+                        logger.info { "test account created." }
+                    }
+                } else {
+                    logger.info { "test account already exist." }
+                }
             }
         }
     }
