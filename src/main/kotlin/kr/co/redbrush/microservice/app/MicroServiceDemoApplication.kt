bba1add3dbf8627ba850a2f8f315cfa3b0ef7659
박경욱 <kwpark@kwpark-MacBook-pro.local>
2019-05-30 21:56:47 +0900

    Enabled ConfigServer and configuration in the application.yml

--- src/main/kotlin/kr/co/redbrush/microservice/app/MicroServiceDemoApplication.kt
+++ src/main/kotlin/kr/co/redbrush/microservice/app/MicroServiceDemoApplication.kt
@@ -2,8 +2,10 @@ package kr.co.redbrush.microservice.app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
+@EnableConfigServer
 class MicroServiceDemoApplication
 
 fun main(args: Array<String>) {
